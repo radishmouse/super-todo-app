bc12a4096c359cc75772e336f78f8cbfea26824b
Chris Aquino <chris.aquino@gmail.com>
2018-06-22 11:00:36 -0400

    adds example calls for create, update, retrieve

---
+++ example-queries.sql
@@ -0,0 +1,39 @@
+
+-- get all the todos
+select * from todos;
+
+-- get one todo by id
+select * from todos
+where id=2;
+
+-- get all pending todos
+select * from todos
+where isDone=false;
+
+-- get all finished todos
+select * from todos
+where isDone=true;
+
+-- search by title, should have 0 results
+select * from todos
+where title ilike '%zzzzzzzzz%';
+
+-- search by title, should have 1 result
+select * from todos
+where title ilike '%scoop%';
+
+
+-- "uncheck" a todo
+update todos
+set isDone=false
+where id=1;
+
+-- "check" a todo
+update todos
+set isDone=true
+where id=2;
+
+-- change title
+update todos
+set title='cook amazing dinner'
+where id=3;
